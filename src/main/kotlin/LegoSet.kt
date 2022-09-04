1e6900f4810038a724206c4b9a85db0735afda0e
Ariel Wang <arielxiewang@gmail.com>
2022-09-04 15:07:58 +1000

    - Add SearchRepository Class - Add LegoSet data class - Add LegoTheme data class - Use find function instead of for loops

---
+++ src/main/kotlin/LegoSet.kt
@@ -0,0 +1,7 @@
+data class LegoSet(
+    val set_id : String,
+    val name: String,
+    val year: String,
+    val theme_id: String,
+    val num_parts: String
+)
---
+++ src/main/kotlin/LegoTheme.kt
@@ -0,0 +1,5 @@
+data class LegoTheme(
+    val id: String,
+    val name: String,
+    val parent_id: String?
+)
